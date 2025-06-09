fd82b9b23ae20db40695b618d2c98f673b5d631e
James Mather Laptop <captaintriton167@gmail.com>
2025-06-09 18:04:51 +0100

    Change: added build settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     react(),
     electron({
